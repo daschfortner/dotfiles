fb5c4accdf75ca2c50cd6f55bd8490ead5305166
Daschel Fortner <daschfortner@gmail.com>
2024-01-31 22:26:49 -0500

    Fix battery script

--- i3blocks/.config/i3blocks/battery.sh
+++ i3blocks/.config/i3blocks/battery.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# echo "B: "
-
 STATUS=$(acpi -b)
 
 PERCENTAGE=$(echo "$STATUS" | grep -Eo "[0-9]+%")
@@ -9,8 +7,17 @@ CHARGING=$(echo "$STATUS" | grep -o "Charging")
 
 if [ "$CHARGING" = "" ]
 then
-  REMAINING=$(echo "$STATUS" | grep -Eo "[0-9]+:[0-9]+")
-  echo "B: $PERCENTAGE ($REMAINING)"
+
+  FULL=$(echo "$STATUS" | grep -o "Full")
+
+  if [ "$FULL" = "Full" ]
+  then
+    echo "B: 100%"
+  else
+    REMAINING=$(echo "$STATUS" | grep -Eo "[0-9]+:[0-9]+")
+    echo "B: $PERCENTAGE ($REMAINING)"
+  fi
+  
 else
   echo "<span color='#6A9955'>B: $PERCENTAGE</span>"
 fi
