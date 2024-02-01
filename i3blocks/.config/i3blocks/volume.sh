a0db02564db999088337270b67d0409079ebb365
Daschel Fortner <daschfortner@gmail.com>
2024-01-31 22:35:03 -0500

    Add mute state to volume block

--- i3blocks/.config/i3blocks/volume.sh
+++ i3blocks/.config/i3blocks/volume.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 PACTL_OUTPUT=$(pactl --format json list sinks)
-VOLUME=$(echo "$PACTL_OUTPUT" | jq 'first(.[].volume[] | .value_percent)')
 
-echo "V: $(echo "$VOLUME" | tr -d '"')"
+if [ "$(echo "$PACTL_OUTPUT" | jq '.[].mute')" = "true" ]
+then
+  echo "V: muted"
+else
+  VOLUME=$(echo "$PACTL_OUTPUT" | jq 'first(.[].volume[] | .value_percent)')
+
+  echo "V: $(echo "$VOLUME" | tr -d '"')"
+fi
+
